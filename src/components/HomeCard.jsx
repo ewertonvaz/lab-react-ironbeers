0400ac3373916a65da6574174fac733f55de8842
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-11-14 17:37:17 -0300

    inicializada a estilização

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -2,11 +2,15 @@ function HomeCard({img, title, content, child}) {
 
     const contentDefault = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum dicta ab voluptatem doloribus tempore itaque voluptatibus, numquam quaerat nihil? Sint deleniti autem quos tenetur recusandae velit, molestiae ratione quibusdam sed";
     return ( 
-        <div style={{display: "flex", flexDirection: "column"}}>
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            border: "1px solid gray"
+        }}>
             <img src={img} alt="" style={{width: "100%"}} />
             <div style={{padding: "16px", display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
-                <h1>{title}</h1>
-                <p style={{textAlign: "left"}}>{content ? content : contentDefault}</p>
+                <h1 style={{marginBottom: "2px"}}>{title}</h1>
+                <p style={{marginTop: "2px", textAlign: "left"}}>{content ? content : contentDefault}</p>
             </div>
         </div>
      );
